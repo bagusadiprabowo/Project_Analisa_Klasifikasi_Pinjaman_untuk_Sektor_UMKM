75f90436d819bd1e5c9e203ca6a52d10bc503f84
Bagus <95150304+bagusadiprabowo@users.noreply.github.com>
2024-02-26 15:00:37 +0700

    Delete Klasifikasi Pinjaman untuk Sektor UMKM.R

--- Klasifikasi Pinjaman untuk Sektor UMKM.R
+++
@@ -1,8 +0,0 @@
-Klasifikasi Pinjaman untuk Sektor UMKM
-
-Membaca Data External
-Info: materi telah diperbarui pada tanggal 19 Agustus 2021, pastikan kembali kode yang telah ditulis disesuaikan dengan bagian Lesson.
-Hal yang pertama dilakukan adalah membaca dataset yang telah dipersiapkan. Dengan menggunakan fungsi bawaan R bacalah dataset yang berformat csv tersebut yang bernama "https://storage.googleapis.com/dqlab-dataset/project.csv".
-Code Editor R :
-data = read.csv("https://storage.googleapis.com/dqlab-dataset/project.csv")
-
